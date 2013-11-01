0c7967410e2143604970a0843c40ba69224312b3
Dany Jupille <dany.jupille@he-arc.ch>
2013-11-01 16:18:44 +0100

    Repository initialization

---
+++ website/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Paint something!</title>
+        <link rel="stylesheet" href="css/general-styles.css">
+        <link rel="stylesheet" href="css/sign-in-styles.css">
+    </head>
+    <body>
+        <div id="top-glue"></div>
+        <div id="sign-in-container"></div>
+    </body>
+</html>
