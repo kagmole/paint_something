1d038355dfdbc0e19a04281c57b8fa75171d8749
Dany Jupille <dany.jupille@he-arc.ch>
2013-12-06 03:10:08 +0100

    Added skeleton for Game module

---
+++ website/module/Game/src/Game/Controller/GameController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Game\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class GameController extends AbstractActionController {
+ 
+    public function indexAction() {
+    
+    }
+}
